855223291d93d0982eafd04d8cbc0dabcd60d0d7
Kevin Hawkins <khawkins@salesforce.com>
2014-06-25 15:45:42 -0700

    Updates for 2.2.1 patch

--- forceios.js
+++ forceios.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var version = '2.1.0',
+var version = '2.2.1',
     exec = require('child_process').exec,
     path = require('path'),
     commandLineUtils = require('./HybridShared/node/commandLineUtils');
